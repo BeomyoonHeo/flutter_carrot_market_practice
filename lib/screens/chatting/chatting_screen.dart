798472f78076953ba7742009ceb8709e15f58a91
BeomyoonHeo <gqd0512@naver.com>
2022-11-21 16:03:32 +0900

    채팅목록 완료

--- lib/screens/chatting/chatting_screen.dart
+++ lib/screens/chatting/chatting_screen.dart
@@ -1,8 +1,21 @@
+import 'package:carrot_market_ui/models/chat_message.dart';
+import 'package:carrot_market_ui/screens/chatting/components/chat_container.dart';
+import 'package:carrot_market_ui/screens/components/appbar_preferred_size.dart';
 import 'package:flutter/material.dart';
 
 class ChattingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold()
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('채팅', style: TextStyle(color: Colors.black)),
+        bottom: appBarBottomLine(),
+      ),
+      body: ListView(
+        children: List.generate(chatMessageList.length, (index) {
+          return ChatContainer(chatMessage: chatMessageList[index]);
+        }),
+      ),
+    );
   }
 }
