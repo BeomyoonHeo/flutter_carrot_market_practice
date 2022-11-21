9e6da01e5dfedd9f674caa2e4026ff8a3d5e48df
BeomyoonHeo <gqd0512@naver.com>
2022-11-21 14:37:32 +0900

    나의 당근 완료

--- lib/screens/chatting/chatting_screen.dart
+++ lib/screens/chatting/chatting_screen.dart
@@ -3,8 +3,6 @@ import 'package:flutter/material.dart';
 class ChattingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('chattingScreen'),
-    );
+    return Scaffold()
   }
 }
